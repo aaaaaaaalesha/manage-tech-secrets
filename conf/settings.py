337958f976fb8a2e9d7a0c995f615539267363d7
aaaaaaaalesha <sks2311211@mail.ru>
2024-12-19 20:02:12 +0300

    docs: update README.md

--- conf/settings.py
+++ conf/settings.py
@@ -33,6 +33,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Third-party apps.
+    'django_extensions',
     # My django-apps.
     'apps.users',
 ]
